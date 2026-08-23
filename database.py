0058ac35ed280aaefc8ab32d20ddb7e764f6e724
pooya <Pooya.parva81@gmail.com>
2026-08-23 17:20:55 +0330

    MVP completed7

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "sqlite:///data/dongi.db"
+#DATABASE_URL = "sqlite:///data/dongi.db"
+DATABASE_URL = "sqlite:////tmp/database.db"
 
 engine = create_engine(
     DATABASE_URL,
